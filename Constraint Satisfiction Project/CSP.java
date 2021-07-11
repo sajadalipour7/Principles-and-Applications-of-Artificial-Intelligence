0c5497cdc5de9d6e25d10194e7b0ce48f80051ae
Sajad <sajadalipour@aut.ac.ir>
2021-07-11 13:42:48 +0430

    Ready for csp

--- Constraint Satisfiction Project/CSP.java
+++ Constraint Satisfiction Project/CSP.java
@@ -1,15 +1,15 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 class Node{
     public int data;
     public int x,y;
-    public ArrayList<Integer> domain;
 
     public Node(int data,int x,int y){
-        domain=new ArrayList<>();
+
         this.data=data;
         this.x=x;
         this.y=y;
@@ -17,7 +17,6 @@ class Node{
 }
 public class CSP {
     static int[][] a;
-    static  ArrayList<Node> variables;
 
     public static void main(String[] args) throws FileNotFoundException {
         File file=new File("./puzzles/puzzle0.txt");
@@ -25,21 +24,34 @@ public class CSP {
         int n=sc.nextInt();
         int dummy=sc.nextInt();
         a=new int[n][n];
-        variables=new ArrayList<Node>();
+        ArrayList<Node> variables=new ArrayList<Node>();
+        HashMap<Node,ArrayList<Integer>> domains=new HashMap<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 String tmp=sc.next();
                 if(tmp.equals("-")){
                     a[i][j]=-1;
+                    Node current=new Node(-1,i,j);
+                    ArrayList<Integer> domain=new ArrayList<>();
+                    domain.add(0);
+                    domain.add(1);
+                    variables.add(current);
+                    domains.put(current,domain);
                 }else{
                     a[i][j]=Integer.parseInt(tmp);
                 }
             }
         }
 
+        cspBacktracking(variables,domains);
+
 
 
     }
+    static void cspBacktracking(ArrayList<Node> variables,HashMap<Node,ArrayList<Integer>> domains){
+        //TODO
+    }
+
 
     static void printArr(int[][] a,int n){
         for(int i=0;i<n;i++){
